434003d1f16a6c72d6171d4f162f9a36cc6db84a
aparicio.ruben <aparicio.ruben@gmail.com>
2013-03-16 18:57:16 +0100

    Index login
    Create a form to made the secure login to access the files.

--- index.php
+++ index.php
@@ -20,19 +20,34 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <?php
+            if(isset($_GET['error'])) { 
+               echo 'Error Logging In!';
+            }
+        ?>
+
+
+        <form action="process_login.php" method="post" name="login_form">
+        	<label for="email">Email: </label><input type="email" name="email" id="email"/ ><br/>
+            <label for="password">Password: </label><input type="password" name="password" id="password"/><br/>
+            <input type="button" value="Login" onClick="formhash(this.form, this.form.password);" />
+        </form>
+        
+        <script type="text/javascript" src="sha512.js"></script>
+		<script type="text/javascript" src="forms.js"></script>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
-
+		<!--
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
+        -->
     </body>
 </html>
